21349a1f763c2f56e485c35c946d78752594f38b
thornel <l.thorne3949@student.leedsbeckett.ac.uk>
2017-03-02 18:18:27 +0000

    Changes made to the parameterised_network_service vulnerability module. A couple of places buffer overflows can be used in the vulnerable program. Fixed the parameterised inputs in the metadata. Modified the install.pp to copy over all the code, compile it and then remove the source code for the vulnerable program.

--- modules/vulnerabilities/unix/misc/parameterised_network_service/manifests/install.pp
+++ modules/vulnerabilities/unix/misc/parameterised_network_service/manifests/install.pp
@@ -1,9 +1,41 @@
 class parameterised_network_service::install {
+	# Create new directory to clean up	
+	file { '/tmp/code':
+		ensure => 'directory',
+  	}	
+	
+	# Copy the code to the VM
+	file { "/tmp/code/c_code.c":
+		ensure => file,
+		content => template('parameterised_network_service/c_code.c.erb')
+	}
+	
+	file { "/tmp/code/authentication_helper.c":
+		ensure => file,
+		content => template('parameterised_network_service/authentication_helper.c')
+	}
 
-  # Apply default CSS template
-  file { "/tmp/c_code.c":
-    ensure => file,
-    content => template('parameterised_network_service/c_code.c.erb')
-  }
+	# Compile the code on the VM
+	exec { "compile /tmp/code":
+		command => "gcc c_code.c -o c_code -fno-stack-protector",
+		cwd => "/tmp/code",
+		path =>  [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
+	}
+	
+	# Move the compiled code
+	exec { "move /tmp/code":
+		command => "cp c_code /tmp/c_code",
+		cwd => "/tmp/code",
+		path =>  [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
+	}
+	
+	# Cleanup
+	exec { 'directory-cleanup':
+		command => 'rm -r code',
+		cwd => "/tmp",
+		path =>  [ '/bin/' ]
+	}
 
 }
+
+
